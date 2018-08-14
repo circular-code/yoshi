e7c2d2527df91a4038665849f0d7208d4ff246e6
Stephanos Malikotsinas (ctrl-s) <stm@intranet.ctrl-s.de>
2018-08-14 09:40:17 +0200

    mailto javascript handling, seperated intro into jumbo and contact, added research

--- app.js
+++ app.js
@@ -2,3 +2,30 @@
 /* jshint -W117 */
 /* jshint -W098 */
 
+setTimeout(function() {
+    var mailTo = $('#mailTo');
+    mailTo.attr('href', decodeURIComponent(mailTo.attr('href')).replace('{at}', '@'));
+},1000);
+
+$('.fold').on('click', function() {
+
+    var target = $(this),
+        targetList = target.find('ul'),
+        icon = target.find('.toggle-icon');
+
+    targetList.stop();
+
+    if (target.hasClass('active')) {
+        targetList.slideUp();
+        target.removeClass('active');
+        icon.removeClass('fa-chevron-up');
+        icon.addClass('fa-chevron-down');
+    }
+    else {
+        targetList.slideDown();
+        target.addClass('active');
+        icon.addClass('fa-chevron-up');
+        icon.removeClass('fa-chevron-down');
+    }
+
+});
